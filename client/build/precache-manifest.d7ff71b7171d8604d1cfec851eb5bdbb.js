9e2df702ab628b0b42ce6233a89657cba52e3ee2
Gaston Fernandez <gaston.fernandez@softvision.com>
2020-06-01 20:22:00 -0300

    build for prod

---
+++ client/build/precache-manifest.d7ff71b7171d8604d1cfec851eb5bdbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df769d1f7fff5522bda794854c043bb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dad300021ecce0c7c50",
+    "url": "/static/css/main.c1dc6f96.chunk.css"
+  },
+  {
+    "revision": "14cfa57b31381ab01ab9",
+    "url": "/static/js/2.d16baed0.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.d16baed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dad300021ecce0c7c50",
+    "url": "/static/js/main.68ba1057.chunk.js"
+  },
+  {
+    "revision": "6a460fdabcc5765b113b",
+    "url": "/static/js/runtime-main.cd281166.js"
+  },
+  {
+    "revision": "66f0dfb448197f61c042ce9a3ce6fd24",
+    "url": "/static/media/cart.66f0dfb4.svg"
+  }
+]);
